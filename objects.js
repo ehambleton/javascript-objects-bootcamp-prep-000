dd01658654f6bd131661679198c68924f7dd5155
Edward Hambleton <ehambleton@me.com>
2017-12-15 03:31:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artist: "songTitle"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-return Object.assign({}, playlist, { [artistName]: songTitle })
+return Object.assign(playlist, { [artistName]: songTitle })
 }
 
 function removeFromPlaylist(playlist, artistName) {
